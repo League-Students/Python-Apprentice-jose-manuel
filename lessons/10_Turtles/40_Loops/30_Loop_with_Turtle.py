f5af1b2be0d69ee6fa329776008916ba8c3520c4
League Student <student@jointheleague.org>
2026-06-25 17:34:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/40_Loops/30_Loop_with_Turtle.py
+++ lessons/10_Turtles/40_Loops/30_Loop_with_Turtle.py
@@ -22,7 +22,7 @@ tina = turtle.Turtle()                  # Create a turtle named tina
 tina.shape('turtle')                    # Set the shape of the turtle to a turtle
 tina.speed(2)                           # Move at a moderate speed, not too fast.
 
-for i in range(5)
+for i in range(5):
 tina.forward(150)
 tina.left(72)
 
